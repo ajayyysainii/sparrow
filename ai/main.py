a7c31264aede1e1a3f69b84c39549377804be96b
Arnav Bhilwariya <arnavbhilwariya0408@gmail.com>
2025-11-01 14:42:38 +0530

    Remove hardcoded secrets and use environment variables

---
+++ ai/main.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+from flask_cors import CORS
+
+load_dotenv()
+
+from app import create_app
+
+app = create_app()
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:8000"}})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, use_reloader=False)
